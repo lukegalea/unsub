e8b2dc9bff91896fa29c871029a7741da867891f
Luke Galea <lukegalea@lgs-pro.local>
2009-10-14 13:32:35 -0400

    Add downcasing of all emails

--- app/models/exclusion.rb
+++ app/models/exclusion.rb
@@ -5,9 +5,14 @@ class Exclusion
   property :site_id, Integer, :nullable => false
   property :email_hash, String, :nullable => false
   property :email, String, :format => :email_address
+
+  property :created_at, DateTime
+
+  validates_is_unique :email, :scope => :site_id
   
   def email=(email)
-    attribute_set(:email, email)    
-    self.email_hash = Digest::MD5.hexdigest(email)
+    down_email = email.downcase
+    attribute_set(:email, down_email)    
+    self.email_hash = Digest::MD5.hexdigest(down_email)
   end
 end
